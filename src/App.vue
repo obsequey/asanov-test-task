1f3f2b6c0d1333bae03378c60d7f90c9420d22db
Artyom Aristov <burnallatonce@localhost.localdomain>
2020-03-21 19:32:11 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="grid">
+    <h1>Hello Worlds</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(72px, 12);
+  column-gap: 32px;
+}
+</style>
